dd391b1e084f4cc8a72757f7775063dac5a7866c
Shawn <121857051@qq.com>
2018-10-08 09:52:05 +0800

    update : simplify the code of ex12 with else

--- 100examples/012.py
+++ 100examples/012.py
@@ -8,3 +8,14 @@ for i in range(100,200):
     if flag:
         continue
     print(i)
+
+
+print('\nSimplify the code with "else"\n')
+
+
+for i in range(100,200):
+    for j in range(2,round(math.sqrt(i))+1):
+        if i%j==0:
+            break
+    else:
+        print(i)
