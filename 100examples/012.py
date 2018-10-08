1a88361ab0b10d5d19f39141537b30c590d8d681
Shawn <121857051@qq.com>
2018-10-08 09:31:39 +0800

    bug fix : in the judge loop, the range should be 2 to round(sqrt(i))+1,or else it can not judge the square number correctly.

--- 100examples/012.py
+++ 100examples/012.py
@@ -1,7 +1,7 @@
 import math
 for i in range(100,200):
     flag=0
-    for j in range(2,round(math.sqrt(i))):
+    for j in range(2,round(math.sqrt(i))+1):
         if i%j==0:
             flag=1
             break
